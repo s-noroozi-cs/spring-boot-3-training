b141c1712898d3966b510937b567e63bb80d8294
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-03-13 22:33:42 +0330

    Create LogInterceptor.java

---
+++ source-codes/spring-graphql/src/main/java/com/springboot3/sample/graphql/interceptor/LogInterceptor.java
@@ -0,0 +1,40 @@
+package com.springboot3.sample.graphql.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.server.WebGraphQlInterceptor;
+import org.springframework.graphql.server.WebGraphQlRequest;
+import org.springframework.graphql.server.WebGraphQlResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LogInterceptor implements WebGraphQlInterceptor {
+
+    @Override
+    public Mono<WebGraphQlResponse> intercept(WebGraphQlRequest request, Chain chain) {
+        final String graphqlRequestLog = """
+                
+                ----- graphql request log interceptor ---
+                headers: %s
+                uri: %s
+                id: %s
+                document: %s
+                execution id: %s
+                operation name: %s
+                variables: %s
+                """.formatted(
+                request.getHeaders().toSingleValueMap(),
+                request.getUri(),
+                request.getId(),
+                request.getDocument(),
+                request.getExecutionId(),
+                request.getOperationName(),
+                request.getVariables()
+        );
+
+        return chain.next(request);
+    }
+
+
+}
