2f1159e23916695b9cee787f999b69bdea4eb820
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-03-14 23:08:48 +0330

    Update CustomerRestController.java

--- source-codes/spring-cloud/customer-service/src/main/java/com/springboot3/sample/services/customer/controller/CustomerRestController.java
+++ source-codes/spring-cloud/customer-service/src/main/java/com/springboot3/sample/services/customer/controller/CustomerRestController.java
@@ -2,6 +2,7 @@ package com.springboot3.sample.services.customer.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/customers")
 public class CustomerRestController {
 
-    @GetMapping
-    public ResponseEntity getCustomer(){
-        return ResponseEntity.ok().build();
+    @GetMapping("/{id}")
+    public ResponseEntity getCustomer(@PathVariable("id") String id){
+        return ResponseEntity.ok("customer with id: " + id);
     }
 }
