8c98555cfa870b2564674bdf9a867de19ff3c84f
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-03-17 22:20:34 +0330

    support circuit breaker

--- source-codes/spring-cloud/common-service/src/main/java/com/springboot3/sample/services/common/api/MerchantApiSignature.java
+++ source-codes/spring-cloud/common-service/src/main/java/com/springboot3/sample/services/common/api/MerchantApiSignature.java
@@ -1,5 +1,6 @@
 package com.springboot3.sample.services.common.api;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,5 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface MerchantApiSignature {
 
     @GetMapping("/api/v1/merchants/{id}")
+    @CircuitBreaker(name = "merchant-service")
     ResponseEntity<String> fetchMerchant(@PathVariable("id") long id);
 }
