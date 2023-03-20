b0ed5b45f0ca0f4a6e7bd9a5006dbff1e7280427
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-03-20 20:58:17 +0330

    add sample - test code to send and receive message - rabbitMQ

--- source-codes/spring-cloud/payment-service/src/main/java/com/springboot3/sample/services/payment/PaymentServiceApplication.java
+++ source-codes/spring-cloud/payment-service/src/main/java/com/springboot3/sample/services/payment/PaymentServiceApplication.java
@@ -1,8 +1,11 @@
 package com.springboot3.sample.services.payment;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,6 +13,16 @@ public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+	@Bean
+	public Queue myQueue() {
+		return new Queue("myQueue", false);
+	}
+
+	@RabbitListener(queues = "myQueue")
+	public void listen(String in) {
+		System.out.println("Message read from myQueue : " + in);
 	}
 
 }
