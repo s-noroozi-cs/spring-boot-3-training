689f624daf10abf5b0c1ed787acc772b8d59da5b
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-03-22 19:22:12 +0330

    remove test code

--- source-codes/spring-cloud/payment-service/src/main/java/com/springboot3/sample/services/payment/PaymentServiceApplication.java
+++ source-codes/spring-cloud/payment-service/src/main/java/com/springboot3/sample/services/payment/PaymentServiceApplication.java
@@ -1,11 +1,8 @@
 package com.springboot3.sample.services.payment;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -14,15 +11,4 @@ public class PaymentServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
 	}
-
-	@Bean
-	public Queue myQueue() {
-		return new Queue("myQueue", false);
-	}
-
-	@RabbitListener(queues = "myQueue")
-	public void listen(String in) {
-		System.out.println("Message read from myQueue : " + in);
-	}
-
 }
--- source-codes/spring-cloud/payment-service/src/main/java/com/springboot3/sample/services/payment/controller/PaymentController.java
+++ source-codes/spring-cloud/payment-service/src/main/java/com/springboot3/sample/services/payment/controller/PaymentController.java
@@ -2,7 +2,6 @@ package com.springboot3.sample.services.payment.controller;
 
 import com.springboot3.sample.services.payment.service.CustomerClient;
 import com.springboot3.sample.services.payment.service.MerchantClient;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,30 +14,25 @@ public class PaymentController {
 
     private CustomerClient customerClient;
     private MerchantClient merchantClient;
-    private RabbitTemplate rabbitTemplate;
 
     public PaymentController(CustomerClient customerClient
-            , MerchantClient merchantClient
-            , RabbitTemplate rabbitTemplate) {
+            , MerchantClient merchantClient) {
         this.customerClient = customerClient;
         this.merchantClient = merchantClient;
-        this.rabbitTemplate = rabbitTemplate;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity fetchPayment(@PathVariable("id") long id) {
 
-//        var response = """
-//                --- response ---
-//                payment service: fetch payment %d was successfully.
-//                customer service: %s
-//                merchant service: %s
-//                """.formatted(id
-//                , customerClient.fetchCustomer(10).getBody()
-//                , merchantClient.fetchMerchant(20).getBody());
+        var response = """
+                --- response ---
+                payment service: fetch payment %d was successfully.
+                customer service: %s
+                merchant service: %s
+                """.formatted(id
+                , customerClient.fetchCustomer(10).getBody()
+                , merchantClient.fetchMerchant(20).getBody());
 
-        rabbitTemplate.convertAndSend("myQueue", "Hello, world!");
-
-        return ResponseEntity.ok("ok");
+        return ResponseEntity.ok(response);
     }
 }
