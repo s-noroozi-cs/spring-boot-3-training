410d92e58e9beae48fb41f45934bf101e9bac75b
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-03-23 09:24:39 +0330

    remove sample code

--- source-codes/spring-cloud/merchant-service/src/main/java/com/springboot3/sample/services/merchant/MerchantServiceApplication.java
+++ source-codes/spring-cloud/merchant-service/src/main/java/com/springboot3/sample/services/merchant/MerchantServiceApplication.java
@@ -2,9 +2,6 @@ package com.springboot3.sample.services.merchant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.function.Consumer;
 
 @SpringBootApplication
 public class MerchantServiceApplication {
@@ -12,13 +9,5 @@ public class MerchantServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MerchantServiceApplication.class, args);
 	}
-
-
-	@Bean
-    public Consumer<String> pkslowSink() {
-        return message -> {
-            System.out.println("Received message " + message);
-        };
-    }
 
 }
