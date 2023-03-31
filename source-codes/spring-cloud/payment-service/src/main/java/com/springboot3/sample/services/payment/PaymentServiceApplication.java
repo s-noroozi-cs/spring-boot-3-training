063ba5fbc2eb9372b0cd5a31d16a96ab254fd197
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-03-31 14:01:11 +0330

    add tracing sample code

--- source-codes/spring-cloud/payment-service/src/main/java/com/springboot3/sample/services/payment/PaymentServiceApplication.java
+++ source-codes/spring-cloud/payment-service/src/main/java/com/springboot3/sample/services/payment/PaymentServiceApplication.java
@@ -1,8 +1,12 @@
 package com.springboot3.sample.services.payment;
 
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,5 +16,9 @@ public class PaymentServiceApplication {
         SpringApplication.run(PaymentServiceApplication.class, args);
     }
 
+    @Bean
+    public Capability capability(final MeterRegistry registry) {
+        return new MicrometerCapability(registry);
+    }
 
 }
