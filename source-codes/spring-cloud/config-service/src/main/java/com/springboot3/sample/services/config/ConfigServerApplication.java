b2d2eeb44d041e47fb1809e2721c9c752f5b3940
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-04-02 00:26:41 +0330

    update security filter and application yaml config

--- source-codes/spring-cloud/config-service/src/main/java/com/springboot3/sample/services/config/ConfigServerApplication.java
+++ source-codes/spring-cloud/config-service/src/main/java/com/springboot3/sample/services/config/ConfigServerApplication.java
@@ -2,12 +2,33 @@ package com.springboot3.sample.services.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
+        try {
+            SpringApplication.run(ConfigServerApplication.class, args);
+        }catch (Throwable ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .ignoringRequestMatchers("/encrypt/**")
+                .ignoringRequestMatchers("/decrypt/**");
+
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin();
+        http.httpBasic();
+        return http.build();
     }
 
 
