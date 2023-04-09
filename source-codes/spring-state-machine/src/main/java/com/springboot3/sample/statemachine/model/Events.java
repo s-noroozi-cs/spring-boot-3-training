1c11ce1e4aa24472dbc33c93095dd989f7b006ca
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-04-09 20:52:56 +0330

    fix circular dependencies

---
+++ source-codes/spring-state-machine/src/main/java/com/springboot3/sample/statemachine/model/Events.java
@@ -0,0 +1,6 @@
+package com.springboot3.sample.statemachine.model;
+
+public enum Events {
+    E1, E2
+}
+
---
+++ source-codes/spring-state-machine/src/main/java/com/springboot3/sample/statemachine/model/States.java
@@ -0,0 +1,5 @@
+package com.springboot3.sample.statemachine.model;
+
+public enum States {
+    SI, S1, S2
+}
