3f417075bcc70ca6e8d54a48b0572c558bff7b90
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-04-11 23:26:13 +0330

    remove varv dependency

--- source-codes/spring-cloud/payment-service/src/main/java/com/springboot3/sample/services/payment/util/FeignFallbackUtil.java
+++ source-codes/spring-cloud/payment-service/src/main/java/com/springboot3/sample/services/payment/util/FeignFallbackUtil.java
@@ -1,12 +1,13 @@
 package com.springboot3.sample.services.payment.util;
 
-import io.vavr.control.Try;
-
 import java.util.function.Supplier;
 
 public class FeignFallbackUtil {
     public static <T> T call(Supplier<T> main, Supplier<T> fallback) {
-        return Try.of(() -> main.get())
-                .fold(ex -> fallback.get(), i -> i);
+        try {
+            return main.get();
+        } catch (Throwable ex) {
+            return fallback.get();
+        }
     }
 }
