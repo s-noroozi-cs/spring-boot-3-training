4b422329646cbfe4fbe3262d863a654d0692b6f7
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-04-18 22:14:30 +0330

    Update StateMachineConfig.java

--- source-codes/spring-state-machine/src/main/java/com/springboot3/sample/statemachine/config/StateMachineConfig.java
+++ source-codes/spring-state-machine/src/main/java/com/springboot3/sample/statemachine/config/StateMachineConfig.java
@@ -14,6 +14,7 @@ import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
 import java.util.EnumSet;
+import java.util.Optional;
 
 @Configuration
 @EnableStateMachine
@@ -62,12 +63,10 @@ public class StateMachineConfig
             }
 
             private String toString(State<States, Events> obj){
-                if(obj == null)
-                    return "null";
-                return "id: %s".formatted(obj.getId()
-                        .describeConstable()
-                        .get()
-                        .toString());
+                return Optional.ofNullable(obj)
+                        .map(State::getId)
+                        .map(String::valueOf)
+                        .orElse("[EMPTY]");
             }
         };
     }
