71ed40ba5384944220c5a9b1a547e781b3130b99
Saeid Noroozi <s.noroozi.cs@gmail.com>
2023-04-19 22:33:59 +0330

    due to the demo session

--- source-codes/spring-cloud/notification-service/src/main/java/com/springboot3/sample/services/notification/listener/NotificationListener.java
+++ source-codes/spring-cloud/notification-service/src/main/java/com/springboot3/sample/services/notification/listener/NotificationListener.java
@@ -5,16 +5,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
 public class NotificationListener {
 
     @Bean
-    public Consumer<Message> notification() {
+    public Consumer<Message<byte[]>> notification() {
         return message -> {
-            log.info("Received new message " + message);
+//            try {
+//                Thread.sleep(2000L);
+//            }catch (Throwable ex){
+//                log.error(ex.getMessage(),ex);
+//            }
+            log.info("Received new message, payload:{}, headers: {}",
+                    new String(message.getPayload(), StandardCharsets.UTF_8),
+                    message.getHeaders()
+                            .entrySet()
+                            .stream()
+                            .map(i -> String.format("%s: %s", i.getKey(), i.getValue()))
+                            .collect(Collectors.joining(", ","[","]"))
+            );
         };
     }
 }
